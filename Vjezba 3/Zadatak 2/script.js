3423b4fc0fb2449ac7c32d48999d5f7ba570123d
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-11-09 22:07:54 +0100

    a
    aa

--- Vjezba 3/Zadatak 2/script.js
+++ Vjezba 3/Zadatak 2/script.js
@@ -1,7 +1,21 @@
-var rezultat;
-for(i = 1; i<=10; i++){
-    for(j = 1; i<=10; j++){
-        rezultat = i * j;
-        document.write(rezultat);
+
+document.write("<table width=\"70%\" height = \"50%\">");
+for(var i = 1; i<=10; i++){
+    if(i % 2 == 0 ){
+    document.write('"<tr style ="background-color: white">');
+    }
+    else{
+        document.write('"<tr style ="background-color: #FFF2CC">');
+    }
+    document.write("<th>" + i + "</th>");
+    for(var j = 1; j<=10; j++){
+        if(i== 1 && j <= 10 ){
+            document.write('"<th style = "background-color: #CCCCCC">' + i*j + "</th>"); 
+        }
+        else{
+            document.write("<td>" + i*j + "</td>"); 
+        }
+    }
+    document.write("</tr>");
 }
-}
+document.write("</table>")
