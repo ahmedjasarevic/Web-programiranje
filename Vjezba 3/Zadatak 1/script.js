f93f25873ceec1d077f6a9a82064b22b8385fc31
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-11-09 20:05:43 +0100

    a

--- Vjezba 3/Zadatak 1/script.js
+++ Vjezba 3/Zadatak 1/script.js
@@ -7,11 +7,19 @@ else
 document.write(odgovor); 
 }
 
+function obrnutiString(str) {
+    var obrnuti = '';
+    for (var i = str.length - 1; i >= 0; i--){
+        obrnuti = obrnuti + str[i];
+}
+return obrnuti;
+}
+
 var odgovor = prompt("Unesi tekst: "); if (odgovor!=null && odgovor!="") 
 { 
 var r=confirm("Pritisnite OK da prikazete tekst u alertboxu a Cancel za prikaz direktno na stranici"); 
 if (r==true) // ili if(r) 
-alert(odgovor); 
+alert(obrnutiString(odgovor));
 else 
-document.write(odgovor); 
+document.write(obrnutiString(odgovor));
 }
---
+++ Vjezba 3/Zadatak 2/script.js
@@ -0,0 +1,7 @@
+var rezultat;
+for(i = 1; i<=10; i++){
+    for(j = 1; i<=10; j++){
+        rezultat = i * j;
+        document.write(rezultat);
+}
+}
