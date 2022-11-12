d3adb154d4db812cf675867240dbac4e41a45b81
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-11-12 16:02:20 +0100

    a

--- Vjezba 4/Zadatak 5/script.js
+++ Vjezba 4/Zadatak 5/script.js
@@ -2,6 +2,7 @@ function promjena(){
 var data = document.getElementById("unos").value;
 var regex = /^(\S*).*\[(.*)\]\s"(\S*)\s(\S*)\s([^"]*)"\s(\S*)\s(\S*)\s"([^"]*)"\s"([^"]*)"$/;
 var zamjena = data.match(regex);
+
 document.getElementById('unos').value = zamjena[2] + " " + zamjena[4] + " " + zamjena[6];
 
 
---
+++ Vjezba 4/Zadatak 6/t4primjer2.js
@@ -0,0 +1,31 @@
+function provjeriFormu() {
+    var greska = document.getElementById('greska');
+    // ili document.body.children[0]
+    greska.innerHTML=""; // ocistimo prethodne greske
+    var forma=document.getElementById('mfid');
+    if (forma.ime.value.length>10 || forma.ime.value.length<3 || forma.prezime.value.length > 20 || forma.prezime.value.length < 3) {
+    greska.innerHTML+="Predugo/prekratko ime<br>";
+    return false;
+    }
+    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var telefonRegEx = /^\(?(\d{3})\)?[-]?(\d{3})[-]?(\d{3})$/;
+    if(!emailRegex.test(forma['email'].value)){
+        greska.innerHTML+="Email format ime@mail.com";
+        return false;
+    }
+
+if (!telefonRegEx.test(forma['telefon'].value)) {
+greska.innerHTML+="Telefon format: (061)-123-345 ili 061-123-456 ili 061123456<br>";
+return false;
+}
+if (!forma.sretan.checked) return false;
+var sel=forma.godina.options[forma.godina.selectedIndex].text;
+// vazan je i .value
+if (sel!=="Druga") return false;
+return true;
+}
+document.getElementById("mfid").addEventListener( "submit",
+provjeriFormu, false );
+document.getElementById("stela").addEventListener( "click", function(ev) {
+document.forms.mojaforma.submit();
+}, false);
