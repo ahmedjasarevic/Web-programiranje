052d0071bf5f12abfef1fbdc1ee4b6593a9c1779
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-11-12 12:16:24 +0100

    a

---
+++ Vjezba 4/Zadatak 1/script.js
@@ -0,0 +1,19 @@
+
+function izPlusauMinus(){
+var x = document.getElementById("prvagodina");
+var x2 = document.getElementById("drugagodina");
+var y = document.getElementById("lista");
+var y2 = document.getElementById("lista2");
+if(y.style.display == "none"){
+    y.style.display = "block";
+}
+else{
+    y.style.display = "none";
+}
+if(y2.style.display == "none"){
+    y2.style.display = "block";
+}
+else{
+    y2.style.display = "none";
+}
+}
