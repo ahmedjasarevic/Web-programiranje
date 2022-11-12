7c4c166b0e3313a7a3b9c99d9f509f13afe8f82d
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-11-12 10:37:14 +0100

    up

---
+++ Vjezba 3/Zadatak 4/script.js
@@ -0,0 +1,15 @@
+
+function dajTekst(){
+var data = document.getElementById("funkcija").value;
+var broj = /\d+/g;
+var rezultat = data.match(broj);
+console.log(rezultat);
+}
+
+function crtaj(){
+    var canvas = document.getElementById("slika");
+    var axes = {};
+var ctx = canvas.getContext("2d");
+return Math.sin(2) ;
+}
+
