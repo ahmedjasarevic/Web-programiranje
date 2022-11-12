82428a31cc9c69aef065127d69aba5ae794a69a4
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-11-12 16:29:40 +0100

    a

--- Vjezba 4/Zadatak 6/t4primjer2.js
+++ Vjezba 4/Zadatak 6/t4primjer2.js
@@ -18,12 +18,16 @@ if (!telefonRegEx.test(forma['telefon'].value)) {
 greska.innerHTML+="Telefon format: (061)-123-345 ili 061-123-456 ili 061123456<br>";
 return false;
 }
+if(forma.password.value != forma.password2.value){
+    greska.innerHTML+="Molimo potvrdite unesenu sifru";
+}
 if (!forma.sretan.checked) return false;
 var sel=forma.godina.options[forma.godina.selectedIndex].text;
 // vazan je i .value
 if (sel!=="Druga") return false;
 return true;
 }
+
 document.getElementById("mfid").addEventListener( "submit",
 provjeriFormu, false );
 document.getElementById("stela").addEventListener( "click", function(ev) {
