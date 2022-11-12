6d71827ce7cf2747cfe6a8f4051b60149863b1dd
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-11-12 18:24:52 +0100

    a

--- Vjezba 4/Zadatak 1/script.js
+++ Vjezba 4/Zadatak 1/script.js
@@ -1,19 +1,9 @@
+var toggler = document.getElementsByClassName("caret");
+var i;
 
-function izPlusauMinus(){
-var x = document.getElementById("prvagodina");
-var x2 = document.getElementById("drugagodina");
-var y = document.getElementById("lista");
-var y2 = document.getElementById("lista2");
-if(y.style.display == "none"){
-    y.style.display = "block";
-}
-else{
-    y.style.display = "none";
-}
-if(y2.style.display == "none"){
-    y2.style.display = "block";
-}
-else{
-    y2.style.display = "none";
-}
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
 }
