9c42250048df01a9ffe3e7bee212bd23c5a65813
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-11-13 10:49:26 +0100

    a

--- Vjezba 4/Zadatak 5/script.js
+++ Vjezba 4/Zadatak 5/script.js
@@ -1,17 +1,20 @@
 function promjena(){
 var data = document.getElementById("unos").value;
 var regex = /(?<src>\d+\.\d+\.\d+\.\d+).+\]\s\"(?<http_method>\w+)\s(?<uri_path>\S+)\s(?<uri_query>\S+)\"\s(?<status>\d+)\s(?<bytes>[\d-]+)/gm;
+var match
 var zamjena = data.match(regex);
-provjeri();
+var duzina = zamjena.length;
+for(var i = 0; i < duzina; i++){
+    if(zamjena[i].match(/\b404\b/g)){
+        document.getElementById('unos').value = zamjena[i];
+}
+else{
+    continue;
+}
+}
 
 
 }
 
 
 
-function provjeri(){
-    if(zamjena.match(/\b404\b/g)){
-       return console.log(zamjena);
-    }
-
-}
