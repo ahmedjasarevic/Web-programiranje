0c530f2c45846ec01c148b04866fdf2ba22269f4
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-11-13 12:49:12 +0100

    a

--- Vjezba 4/Dodatni/Zadatak 9/script.js
+++ Vjezba 4/Dodatni/Zadatak 9/script.js
@@ -1,6 +1,6 @@
 function pretvori(){
-var data = document.getElementById("unosStepeni").value;
+var data = document.getElementById('unosStepeni').value;
 var rezultat = data * 9 / 5 + 32;
-document.getElementById("unosStepeni") = data;
+document.getElementById('unosStepeni').value = rezultat;
 
 }
