621b99790e16d2c746279bf82c5cd7fa3b6b4d83
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-11-13 12:42:03 +0100

    a

---
+++ Vjezba 4/Dodatni/Zadatak 8/script.js
@@ -0,0 +1,6 @@
+function promjeniText(){
+    document.getElementById("demo").innerHTML = "Aktivan";
+  }
+  function promjeniText2(){
+    document.getElementById("demo").innerHTML = "Nije aktivan" ;
+  }
---
+++ Vjezba 4/Dodatni/Zadatak 9/script.js
@@ -0,0 +1,6 @@
+function pretvori(){
+var data = document.getElementById("unosStepeni").value;
+var rezultat = data * 9 / 5 + 32;
+document.getElementById("unosStepeni") = data;
+
+}
