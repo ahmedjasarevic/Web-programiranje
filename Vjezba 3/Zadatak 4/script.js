235194dc67fc5720e2dd09c84f56318ea3a15b4b
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-11-17 21:06:57 +0100

    a

--- Vjezba 3/Zadatak 4/script.js
+++ Vjezba 3/Zadatak 4/script.js
@@ -9,25 +9,40 @@ jednacina(rezultat);
 
 
 function jednacina(rezultat){
-    var epsilon = 0.001;
-for(var x = -100; x < 100; x+=0.001){
-if(math.abs(+rezultat[0]*math.pow(x,+rezultat[1])+ +rezultat[2]+ +rezultat[3]) < epsilon){
-console.log(x);
-}
-}
-}
-
-
-function crtaj(){
   const canvas = document.getElementById('slika');
   if (canvas.getContext) {
-    const ctx = canvas.getContext('2d');
+      const ctx = canvas.getContext('2d');
+      ctx.beginPath();
+      
+      var korak = 0.1
 
-    ctx.beginPath();
-    ctx.moveTo(75, 50);
-    ctx.lineTo(100, 75);
-    ctx.lineTo(100, 25);
-    ctx.fill();
+      // Koordinatni sistem
+      ctx.moveTo(100, 200);
+      ctx.lineTo(100, 0);
+      ctx.moveTo(0, 100);
+      ctx.lineTo(200, 100);
+      
+      // Pocetna tacka je u (0, yp), ali je yp izvan okvira canvasa pa sam ostavio u (0,0)
+      ctx.moveTo(0, 0);
+
+      // Pocetak crtanja na intervalu [-5, 5]
+      // Funkija y = x(x-2.5)
+      for(var x = -5; x < 5; x += korak) {
+          var y = x * x - 2.5 * x // x^2 - 2.5x = x(x-2.5) pa su nule u x=0 i x=2.5
+          
+          // X-osa
+          // Skaliranje: za x = -5, x' = 0
+          // Skaliranje: za x = 5, x' = 200
+          // Zato sam stavio (x+5)*20
+
+          // Y-osa
+          // Znamo da je transformacija y -> y': y' := -y (obrnuto okrenuta)
+          // Skaliranje: za y=-5, y'=200
+          // Skaliranje: za y=5, y'=0 
+          ctx.lineTo(20 * (x + 5),  20 * (-1*(y - 5)));
+          ctx.moveTo(20 * (x + 5),  20 * (-1*(y - 5)));
+          ctx.stroke();
+      }
   }
 }
 
