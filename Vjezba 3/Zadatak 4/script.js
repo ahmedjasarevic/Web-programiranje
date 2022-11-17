ce38e67645dfc8390f246350b5bf7f79a1733dbe
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-11-17 11:11:58 +0100

    a

--- Vjezba 3/Zadatak 4/script.js
+++ Vjezba 3/Zadatak 4/script.js
@@ -1,18 +1,33 @@
 
 function dajTekst(){
 var data = document.getElementById("funkcija").value;
-var broj = /\d+/g;
+var broj = /-?\d+/g;
 var rezultat = data.match(broj);
 console.log(rezultat);
-crtaj();
+jednacina(rezultat);
 }
 
+
+function jednacina(rezultat){
+    var epsilon = 0.001;
+for(var x = -100; x < 100; x+=0.001){
+if(math.abs(+rezultat[0]*math.pow(x,+rezultat[1])+ +rezultat[2]+ +rezultat[3]) < epsilon){
+console.log(x);
+}
+}
+}
+
+
 function crtaj(){
-    var c = document.getElementById("slika");
-var ctx = c.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 200);
-ctx.stroke();
- 
+  const canvas = document.getElementById('slika');
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d');
+
+    ctx.beginPath();
+    ctx.moveTo(75, 50);
+    ctx.lineTo(100, 75);
+    ctx.lineTo(100, 25);
+    ctx.fill();
+  }
 }
 
