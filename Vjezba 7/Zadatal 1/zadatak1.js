0583103538f2e4cec5a8dc3cad09f12bd4b72846
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-12-25 19:46:36 +0100

    a

--- Vjezba 7/Zadatal 1/zadatak1.js
+++ Vjezba 7/Zadatal 1/zadatak1.js
@@ -1,30 +1,22 @@
-const http = require('http');
-const fs = require('fs');
-const port = 8085;
+const express = require('express');
+const bodyParser = require('body-parser');
 
-http.createServer((req, res) => {
-  if (req.url === '/unos') {
-    if (req.method === 'POST') {
-      let body = '';
-      req.on('data', chunk => {
-        body += chunk.toString();
-      });
-      req.on('end', () => {
-        console.log(body);
-        res.end();
-      });
-    } else {
-      fs.readFile('forma.html', (err, data) => {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-      });
-    }
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    res.write('404 Not Found');
-    res.end();
-  }
-}).listen(port);
+const app = express();
 
-console.log(`Server listening on port ${port}`);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/unos', (req, res) => {
+  res.sendFile(__dirname + '/forma.html');
+});
+
+app.post('/', (req, res) => {
+  const name = req.body.name;
+  const lastname = req.body.lastname;
+  const address = req.body.address;
+  const number = req.body.number;
+  res.send(`Ime: ${name} Prezime: ${lastname} Adresa: ${address} Broj telefona: ${number}`);
+});
+
+app.listen(8085, () => {
+  console.log('Server listening on port 8085');
+});
